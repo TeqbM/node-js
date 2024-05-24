b6917dd6d69384a9e90c3b42c9e080aebde21501
TeqbM <mukesh@teqbuddies.com>
2024-05-24 19:09:50 +0530

    route create in node js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const http = require('http');
 
-http.createServer(function(req, res){
-     res.end(`<h1> node js</h1>`)
-}).listen(3030)
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+     res.end(`<h1>This is a home page </h1>  <a href="/about">About us</a>`);
+})
+app.get('/about', (req, res) => {
+     res.end(`<h1>this is a About us page </h1> <a href="/blog">Go to blog page</a>`)
+})
+app.get('/blog', (req, res) => {
+     res.end(`<h1>this is a blog</h1> <a href="/">go to home </a>`)
+})
+
+app.listen(3030)
