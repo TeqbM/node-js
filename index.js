a29489ffa8c0a3b74938d089d8a3cf2d13527afe
TeqbM <mukesh@teqbuddies.com>
2024-05-29 19:13:58 +0530

    file create and rest api create

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
-const http = require('http');
-
-const express = require('express');
+const express = require("express");
 const app = express();
+const fs = require("fs");
+const PORT_URL = 3030;
+const users = require("./users.json");
 
-app.get('/', (req, res) => {
-     res.end(`<h1>This is a home page </h1>  <a href="/about">About us</a>`);
-})
-app.get('/about', (req, res) => {
-     res.end(`<h1>this is a About us page </h1> <a href="/blog">Go to blog page</a>`)
-})
-app.get('/blog', (req, res) => {
-     res.end(`<h1>this is a blog</h1> <a href="/">go to home </a>`)
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function(req, res) {
+  const userList = users.map(user => `<li>${user.name}</li>`);
+  res.send(`${userList.join("")}`);
 })
 
-app.listen(3030)
+
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+
+app.get("/api/users/:id", (req, res) => {
+  let id = Number(req.params.id);
+  let user = users.find((user) => user.id == id);
+  
+  if (id > users.length) {
+    return res.json([{"error": `Id ${id} is not found`}]);
+  }
+  return res.json(user);
+});
+
+app.post("/api/users/", (req, res) => {
+  // create user
+  let body=req.body;
+    users.push({...body, id:users.length+1});
+    fs.writeFile('./users.json',JSON.stringify(users), (err,data) =>{
+      return res.json(users);
+    });
+})
+
+app.patch("/api/users/:id", (req, res) => {
+  let id = Number(req.params.id);
+  let user = users.find((user) => user.id == id);
+  // console.log();
+  if(id> users.length){
+    return res.json({"error": `Id:- ${id} it not found`});
+  }
+  return res.json(user);
+})
+
+app.delete("/api/users/:id", (req, res) => {
+  let id = Number(req.params.id);
+  let user = users.filter(user => user.id !== id);
+  fs.writeFile('./users.json',JSON.stringify(user), (err,data) =>{
+    return res.json(data);
+  });
+})
+
+app.listen(PORT_URL, (req, res) => {
+  console.log(`port run successfully ${PORT_URL}`);
+});
